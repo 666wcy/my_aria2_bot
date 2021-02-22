698678961880918111148649d8a51c8a201f6db3
666wcy <769020367@qq.com>
2021-02-22 11:26:17 +0800

    Update web.py

--- bot/web.py
+++ bot/web.py
@@ -7,7 +7,7 @@ status =""
 app = Flask(__name__)
 SITE_NAME = 'http://127.0.0.1:8080/'
 
-@app.route('/jsonrpc/',methods=['POST'])
+@app.route('/jsonrpc',methods=['POST'])
 def proxypost():
     path="jsonrpc"
     #print("post")
@@ -61,4 +61,4 @@ def proxyget():
 if __name__ == '__main__':
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
